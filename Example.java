302ab16074edf5e7a0e0fc5ddb1056d5a613c360
WieHyunSeo <joak9635@gmail.com>
2026-09-10 10:42:58 +0900

    Implement height method

--- Example.java
+++ Example.java
@@ -4,14 +4,20 @@ public class Example {
         Node root = new Node("Root");
         Node child1 = new Node("Child 1");
         Node child2 = new Node("Child 2");
+        Node child1_1 = new Node("Child 1.1");
 
         root.addChild(child1);
         root.addChild(child2);
+        child1.addChild(child1_1);
 
         System.out.println("Root label: " + root.label);
         System.out.println("Children of Root:");
         for (Node child : root.children) {
             System.out.println("- " + child.label);
         }
+
+        System.out.println("Root height: " + root.height());
+        System.out.println("Child 1 height: " + child1.height());
+        System.out.println("Child 2 height: " + child2.height());
     }
 }
--- Node.java
+++ Node.java
@@ -14,4 +14,18 @@ public class Node {
     public void addChild(Node child) {
         this.children.add(child);
     }
+
+    public int height() {
+        if (children.isEmpty()) {
+            return 0;
+        }
+
+        int maxHeight = 0;
+
+        for (Node child : children) {
+            maxHeight = Math.max(maxHeight, child.height());
+        }
+
+        return maxHeight + 1;
+    }
 }
